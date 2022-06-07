bb5b9d2cb610cf9884b4083567ba6342b33fff6d
kevin4dhd <kevinpiazzolistudio@gmail.com>
2022-06-07 08:13:37 -0500

    xd

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Proyectos\google_map_location_picker\example"
+export "FLUTTER_ROOT=C:\Users\Usuario\Documents\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Usuario\OneDrive\Documents\Proyectos\google_map_location_picker\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
@@ -10,4 +10,4 @@ export "FLUTTER_BUILD_NUMBER=1.0.0"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=false"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.packages"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
